5733eb6804122fc81ebcf8812f1b4d0213f8058f
Himakshi Salhotra <salhotrahimakshi25@gmail.com>
2017-08-14 16:56:13 +0530

    JavaDoc  Practice
    go to project 
    generate javadoc

---
+++ LabWork/src/JavaDoc.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @author Himakshi
+ * @version 10.1
+ * <h1>Java Laboratory</h1>
+ *  <p> I am the student of <b>NIT Jalandhar</b> pursuing BTECH
+ *  CSE. In java lab we will interact with advanced tools in programming and get to learn new features</p>
+ *  
+ *  @see <a href="https://www.facebook.com/"> <b> Facebook </b> </a>
+ *  @see <a href="C:\Users\vk027\Documents\wed develop\index.html"> web designing </a>
+ *  
+ */
+public class JavaDoc 
+{
+	public static void main(String[]args)
+	{
+		JavaDoc obj=new JavaDoc ();
+		System.out.println(obj.func(10));
+		System.out.println("Child Code 2");
+	}
+	/**
+	 * 
+	 * @param a
+	 * @return m
+	 */
+	int func(int a)
+	{
+		int m=5;
+		return(m);
+	}
+	/**
+	 * 
+	 * @param a
+	 * @param b
+	 * @return d
+	 */
+	public double add(double a,double b)
+	{
+		int c=10;
+		double d=a+b+c;
+		return(d);
+	}
+}
