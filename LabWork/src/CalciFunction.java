6754d4269fa370d947d55256a453f642f61cbf81
Himakshi Salhotra <salhotrahimakshi25@gmail.com>
2017-09-11 15:36:47 +0530

    Calculator using interface

---
+++ LabWork/src/CalciFunction.java
@@ -0,0 +1,11 @@
+public interface CalciFunction 
+{
+	public void addition(int a,int b);
+	public void subtract(int a,int b);
+	public void multi(int a,int b);
+	public void division(int a,int b);
+	public void maxNum(int a,int b);
+	public void minNum(int a,int b);
+	public void sin(double a);
+	public void cos(double a);
+}
